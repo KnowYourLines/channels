6ce98e98f2ffd4c3ae1957708a47e07e08c0352e
KnowYourLines <knowyourlines@outlook.com>
2021-05-27 23:19:01 +0100

    Fetch Django ASGI application early to ensure AppRegistry is populated

--- server/asgi.py
+++ server/asgi.py
@@ -9,17 +9,19 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+from django.core.asgi import get_asgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 
 import api.routing
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         # Just HTTP for now. (We can add other protocols later.)
         "websocket": AuthMiddlewareStack(URLRouter(api.routing.websocket_urlpatterns)),
     }
