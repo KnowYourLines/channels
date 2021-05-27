eaea2c31fc46506d3a025ec0203fed19fdb276af
KnowYourLines <knowyourlines@outlook.com>
2021-05-27 13:10:26 +0100

    added room view

--- api/views.py
+++ api/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "api/index.html")
+
+
+def room(request, room_name):
+    return render(request, "api/room.html", {"room_name": room_name})
