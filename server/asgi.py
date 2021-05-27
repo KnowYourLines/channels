080dc063a8f4e49f02336c99268dac730ea03034
KnowYourLines <knowyourlines@outlook.com>
2021-05-27 23:08:29 +0100

    declare settings module first to make heroku happy

--- server/asgi.py
+++ server/asgi.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 import api.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
 application = ProtocolTypeRouter(
     {
