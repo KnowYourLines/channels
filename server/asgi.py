aac70e4680adf4fab37719323d9001befac9eada
Johnny <knowyourlines@outlook.com>
2021-07-27 03:36:35 +0100

    fix order to make heroku happy

--- server/asgi.py
+++ server/asgi.py
@@ -12,11 +12,10 @@ import os
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from api.authentication import TokenAuthMiddlewareStack
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 django_asgi_app = get_asgi_application()
 
+from api.authentication import TokenAuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 import api.routing
